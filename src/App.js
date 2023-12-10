56d1c7f7677d025c707dd8e2c968d3afd38e8150
MatthewYelkin <youtubetv5@mail.ru>
2023-12-11 02:14:21 +0300

    feat: fix structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
+import Header from "./components/Header";
+import "./styles/App.css"
+import SingerOfDay from "./components/SingerOfDay/SingerOfDay";
 
 function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header></Header>
+            <SingerOfDay></SingerOfDay>
+        </div>
+    );
 }
 export default App;
