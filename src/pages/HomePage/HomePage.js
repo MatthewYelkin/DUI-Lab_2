a5f5821be588d4d66f00fa93a7e18c2fa6547651
MatthewYelkin <youtubetv5@mail.ru>
2023-12-11 21:38:07 +0300

    feat: create page - Home

---
+++ src/pages/HomePage/HomePage.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import SingerOfDay from "../../components/SingerOfDay/SingerOfDay";
+import Developers from "../../components/Developers/Developers";
+import "./HomePage.css";
+import AboutPlatform from "../../components/AboutPlatform/AboutPlatform";
+
+const HomePage = () => {
+    return (
+        <div>
+            <AboutPlatform/>
+            <div className="main-section">
+                <SingerOfDay/>
+                <Developers/>
+            </div>
+        </div>
+    );
+};
+
+export default HomePage;
