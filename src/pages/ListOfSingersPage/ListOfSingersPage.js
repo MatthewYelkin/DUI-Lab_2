54e428e770fdf1b1b3f445d7eac2da594d68a5a5
MatthewYelkin <youtubetv5@mail.ru>
2023-12-12 02:59:53 +0300

    feat: add page with all singers

---
+++ src/pages/ListOfSingersPage/ListOfSingersPage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import SingerBlock from "../../components/SingerBlock/SingerBlock";
+import "./ListOfSingersPage.css"
+
+const ListOfSingersPage = () => {
+    return (
+        <div className="ListOfSingersPage">
+            <SingerBlock/>
+            <SingerBlock/>
+            <SingerBlock/>
+            <SingerBlock/>
+
+        </div>
+    );
+};
+
+export default ListOfSingersPage;
