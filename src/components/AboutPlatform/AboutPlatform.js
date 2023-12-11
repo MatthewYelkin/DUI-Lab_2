b99bd5ec93a3a92131a5ff93837251f3d0be2dd6
MatthewYelkin <youtubetv5@mail.ru>
2023-12-11 21:35:49 +0300

    feat: add section About

---
+++ src/components/AboutPlatform/AboutPlatform.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import "./AboutPlatform.css"
+
+const AboutPlatform = () => {
+    return (
+        <div className="AboutPlatform">
+            <div className="AboutPlatform_left">
+                <h1>Данный портал о музыкальных исполнителях</h1>
+            </div>
+            <div className="AboutPlatform_right">
+                <p>Добро пожаловать в захватывающий мир звуков и творчества — ваш источник вдохновения, страсти и невероятного музыкального разнообразия!<br/><br/>Наш сайт - это путеводитель в уникальную вселенную талантливых музыкальных исполнителей, где каждая нота, каждый текст, каждое исполнение — это история, которую стоит рассказать.</p>
+            </div>
+        </div>
+    );
+};
+
+export default AboutPlatform;
