39b612225847336559408d1da7bc8fc7f5200029
MatthewYelkin <youtubetv5@mail.ru>
2023-12-11 21:40:38 +0300

    fix: delete unused imports

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from "react";
 import Header from "./components/Header/Header";
 import "./styles/App.css"
-import SingerOfDay from "./components/SingerOfDay/SingerOfDay";
-import Developers from "./components/Developers/Developers";
 import HomePage from "./pages/HomePage/HomePage";
 import Footer from "./components/Footer/Footer";
 
