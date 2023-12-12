79a518226324a9f5a1acca18703f212b55e04ced
MatthewYelkin <youtubetv5@mail.ru>
2023-12-13 00:38:13 +0300

    feat: add search and title of page

--- src/pages/ListOfSingersPage/ListOfSingersPage.js
+++ src/pages/ListOfSingersPage/ListOfSingersPage.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import SingerBlock from "../../components/SingerBlock/SingerBlock";
 import "./ListOfSingersPage.css"
+import {Form} from "react-bootstrap";
 
 const ListOfSingersPage = () => {
     return (
         <div className="ListOfSingersPage">
-            <SingerBlock/>
-            <SingerBlock/>
-            <SingerBlock/>
-            <SingerBlock/>
-
+            <div className="ListOfSingersPage_header">
+                <h1>Все исполнители</h1>
+                <div className="search_wrapper">
+                    <Form className="search-form">
+                        <Form.Control className="search-box" type="search" placeholder=""/>
+                    </Form>
+                </div>
+            </div>
+            <div className="Singers">
+                <SingerBlock name="OG BUDA" years="1999 - 2023" info="fadsasdfdsfds" to="/singers/ogbuda"/>
+                <SingerBlock name="OG BUDA" years="1999 - 2023" info="fadsasfdsfdsfdsfdsfdsfdsfdsfds fdsfdsfdsfds fdsfds afdsafdsafdsafdsafadsdfdsfds"/>
+                <SingerBlock name="OG BUDA" years="1999 - 2023" info="fadsasdfdsfds"/>
+                <SingerBlock name="OG BUDA" years="1999 - 2023" info="fadsasdfdsfds"/>
+            </div>
         </div>
     );
 };
