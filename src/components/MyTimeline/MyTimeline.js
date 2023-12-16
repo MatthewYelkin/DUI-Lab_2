27d231a6ee498b1de531f2bdf638b72685d16e6c
MatthewYelkin <youtubetv5@mail.ru>
2023-12-16 19:09:54 +0300

    fix: add information

--- src/components/MyTimeline/MyTimeline.js
+++ src/components/MyTimeline/MyTimeline.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import {VerticalTimeline, VerticalTimelineElement} from "react-vertical-timeline-component";
 import 'react-vertical-timeline-component/style.min.css';
+import "./MyTimeline.css";
 
-const MyTimeline = ({id, props}) => {
+const MyTimeline = ({discography}) => {
     return (
         <VerticalTimeline>
-            <VerticalTimelineElement
-                date = "2000"
-                dateClassName = "date-color"
-                className="vertical-timeline-element--work"
-                contentStyle={{ background: 'rgba(24,24,24,0.69)', color: '#fff' }}
-                contentArrowStyle={{ borderRight: '7px solid  #181818' }}
-                iconStyle={{ background: 'rgba(24,24,24,0.69)', color: '#fff' }}
-            >
-                <p>fdsfdsfdsfds</p>
-            </VerticalTimelineElement>
+            {discography.map(({year, info})=>
+                <VerticalTimelineElement
+                    date = {year}
+                    dateClassName = "date-color"
+                    className="vertical-timeline-element--work"
+                    contentStyle={{ background: 'rgba(24,24,24,0.69)', color: '#fff' }}
+                    contentArrowStyle={{ borderRight: '7px solid  #181818' }}
+                    iconStyle={{ background: 'rgba(24,24,24,0.69)', color: '#fff' }}
+                >
+                    <p>{info}</p>
+                </VerticalTimelineElement>
+            )}
         </VerticalTimeline>
     );
 };
