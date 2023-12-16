281b01642cdf82481aba15b72d96d5514a834c6a
MatthewYelkin <youtubetv5@mail.ru>
2023-12-16 19:14:15 +0300

    feat: add translation func

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,11 +2,10 @@ import React from 'react';
 import logo from '../../img/logo.png'
 import {Container, Nav, Navbar, NavDropdown} from "react-bootstrap";
 import {Link} from "react-router-dom";
+import {useTranslation} from "react-i18next";
 
 const Header = () => {
-    const mes = () => {
-        console.log("Translate")
-    }
+    const {t, i18n} = useTranslation();
 
     return (
         <Navbar expand="lg" className="bg-body-tertiary">
@@ -22,12 +21,12 @@ const Header = () => {
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                        <Link to="/" className="nav-link" style={{color:"#181818", fontSize:"20px"}}>Главная</Link>
-                        <Link to="/singers" className="nav-link" style={{color:"#181818", fontSize:"20px"}}>Исполнители</Link>
+                        <Link to="/" className="nav-link" style={{color:"#181818", fontSize:"20px"}}>{t('header_homePage')}</Link>
+                        <Link to="/artists" className="nav-link" style={{color:"#181818", fontSize:"20px"}}>{t('header_singersPage')}</Link>
                     </Nav>
-                    <NavDropdown style={{color:"#181818", fontSize:"20px"}} title="Язык" id="basic-nav-dropdown">
-                        <NavDropdown.Item href="">Английский</NavDropdown.Item>
-                        <NavDropdown.Item href="">Русский</NavDropdown.Item>
+                    <NavDropdown style={{color:"#181818", fontSize:"20px"}} title={t('header_language')} id="basic-nav-dropdown">
+                        <NavDropdown.Item onClick={()=>i18n.changeLanguage("en")}>{t('header_english')}</NavDropdown.Item>
+                        <NavDropdown.Item onClick={()=>i18n.changeLanguage("ru")}>{t('header_russian')}</NavDropdown.Item>
                     </NavDropdown>
                 </Navbar.Collapse>
             </Container>
