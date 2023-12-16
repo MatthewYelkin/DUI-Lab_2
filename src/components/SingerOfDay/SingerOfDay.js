37a1910480d75cc744f101c038f53444711b9d2b
MatthewYelkin <youtubetv5@mail.ru>
2023-12-16 19:10:08 +0300

    fix: add information

--- src/components/SingerOfDay/SingerOfDay.js
+++ src/components/SingerOfDay/SingerOfDay.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import "./SingerOfDay.css";
 import SingerBlock from "../SingerBlock/SingerBlock";
+import {useTranslation} from "react-i18next";
+import singers from "../../data/singers.json"
 
 const SingerOfDay = () => {
+    const {t, i18n} = useTranslation()
+
+    const singer = Object.keys(singers)[Math.floor(Math.random() * 5)]
+
     return (
         <div className = "SingerOfDay">
-            <h1>Исполнитель дня</h1>
+            <h1>{t('singerOfDay_singerOfDay')}</h1>
             <div>
-                <SingerBlock name="OGBUDA" years="1999 - 2023" info="fadsasdfdsfds" style ={{marginTop: "-120px"}}/>
+                <SingerBlock
+                    img={`${singer}.png`}
+                    name={singers[singer]["nick"]}
+                    years={t(`singers.${singer}.birth_date`)}
+                    info={t(`singers.${singer}.short_info`)}
+                    page={`/artists/${singer}`}
+                    style ={{marginTop: "-120px"}}
+                />
             </div>
         </div>
     );
