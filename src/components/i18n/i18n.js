610cae456cf0fd8125d7beae23c7e480236e5db0
MatthewYelkin <youtubetv5@mail.ru>
2023-12-16 19:08:59 +0300

    feat: add i18n

---
+++ src/components/i18n/i18n.js
@@ -0,0 +1,20 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import HttpApi from "i18next-http-backend";
+import Backend from 'i18next-http-backend';
+
+    i18n.use(initReactI18next).use(HttpApi).use(Backend).init({
+        debug: true,
+        lng: "ru",
+        supportedLngs: ["en", "ru"],
+
+        interpolation: {
+            escapeValue: false
+        },
+
+        backend: {
+            loadPath: '/locales/{{lng}}/{{ns}}.json'
+        }
+    });
+
+export default i18n;
